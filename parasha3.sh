23924e25cd6647c5c5f32e9fd212478711013cbb
OneDub <60428639+xtrastadia@users.noreply.github.com>
2022-07-29 00:30:30 +1000

    Create parasha3.sh

---
+++ parasha3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+export GPU_MAX_HEAP_SIZE=100
+export GPU_MAX_USE_SYNC_OBJECTS=1
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+export GPU_ENABLE_LARGE_ALLOCATION=100
+export GPU_MAX_WORKGROUP_SIZE=1024
+
+unset LD_PRELOA
+unset LD_PRELOAD_ENV
+unset LD_LIBRARY_PATH
+./4b --algorithm cryptonight_gpu --pool de.conceal.herominers.com:1115 --wallet ccx7Q3TFtw5WBQeCoRn1NGZkqtdDR8skSSsouqUfmNnBHc5dw833orGVrvXyvhWZTP8EPwm4vGa72RXFiQEDbGA61xwYRC2pBJ --password testnet
