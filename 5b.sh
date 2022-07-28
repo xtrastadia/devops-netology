4c52655f1a7d74f36ade37fc91c1b3d56ad704b3
OneDub <60428639+xtrastadia@users.noreply.github.com>
2022-07-29 00:14:11 +1000

    Add files via upload

---
+++ 5b.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+export GPU_MAX_HEAP_SIZE=100
+export GPU_MAX_USE_SYNC_OBJECTS=1
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+export GPU_ENABLE_LARGE_ALLOCATION=100
+export GPU_MAX_WORKGROUP_SIZE=1024
+
+./4b --disable-cpu --algorithm ethash cryptonight_gpu de.conceal.herominers.com:1115  --wallet ccx7Q3TFtw5WBQeCoRn1NGZkqtdDR8skSSsouqUfmNnBHc5dw833orGVrvXyvhWZTP8EPwm4vGa72RXFiQEDbGA61xwYRC2pBJ --password testnet
